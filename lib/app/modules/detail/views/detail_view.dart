3a32472f391c8e7b58ad95173ef5d26a5a61839c
MitkuDubale38 <mitkudubale3864@gmail.com>
2023-12-31 13:06:35 +0300

    page viewer progress

--- lib/app/modules/detail/views/detail_view.dart
+++ lib/app/modules/detail/views/detail_view.dart
@@ -5,27 +5,53 @@ import 'package:bible_book_app/app/utils/helpers/text_viewer/text_viewer_page.da
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:page_flip/page_flip.dart';
 
 import '../controllers/detail_controller.dart';
 
 class DetailView extends GetView<DetailController> {
   DetailView({Key? key}) : super(key: key);
   final themeData = Get.find<ThemeController>().themeData.value;
+
+  final _controller = GlobalKey<PageFlipWidgetState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: TextViewerPage(
-      
-          textViewer: TextViewer.asset(
-            'assets/books/exodus/exodus.1.txt',
-            highLightColor: Colors.yellow,
-            focusColor: themeData!.primaryColor.withOpacity(0.7),
-            ignoreCase: true,
-            
-          ),
-          // showSearchAppBar: true,
+        child: PageFlipWidget(
+          key: _controller,
+          backgroundColor: Colors.white,
+          // isRightSwipe: true,
+          lastPage: Container(
+              color: Colors.white,
+              child: const Center(child: Text('Last Page!'))),
+          children: <Widget>[
+            TextViewerPage(
+              textViewer: TextViewer.asset(
+                'assets/books/exodus/exodus.1.txt',
+                highLightColor: Colors.yellow,
+                focusColor: themeData!.primaryColor.withOpacity(0.7),
+                ignoreCase: true,
+              ),
+              // showSearchAppBar: true,
+            ),
+            TextViewerPage(
+              textViewer: TextViewer.asset(
+                'assets/books/exodus/exodus.1.txt',
+                highLightColor: Colors.yellow,
+                focusColor: themeData!.primaryColor.withOpacity(0.7),
+                ignoreCase: true,
+              ),
+              // showSearchAppBar: true,
+            ),
+          ],
         ),
+        // floatingActionButton: FloatingActionButton(
+        //   child: const Icon(Icons.looks_5_outlined),
+        //   onPressed: () {
+        //     _controller.currentState?.goToPage(5);
+        //   },
+        // ),
       ),
     );
   }
